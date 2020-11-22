6b53ddbb8d9aa535965fbc8581e0a2e4502f64d3
Jamal Hasuev <metafra@mail.ru>
2020-11-22 16:53:40 +0300

    Adding tochmove support

--- src/components/Movement.js
+++ src/components/Movement.js
@@ -1,6 +1,5 @@
 import {
   canvas_user as canvas,
-  ctx_user as ctx
 } from "./canvas.js"
 
 export default new function(){
@@ -8,19 +7,34 @@ export default new function(){
   this.y = 0
   this.styles = {}
 
-  canvas.addEventListener("mousemove", (event) => {
+  this.setPositions = (event) => {
     let canvasWidth = this.styles.width
     let canvasHeight = this.styles.height
 
-    let percentCursorX = event.layerX / canvasWidth * 100
-    let percentCursorY = event.layerY / canvasHeight * 100
+    let layerX,
+        layerY;
+
+    if (event.type == 'touchmove') {
+      let touch = event.touches[0]
+      layerX = touch.clientX - touch.target.offsetLeft
+      layerY = touch.clientY - touch.target.offsetTop
+    } else {
+      layerX = event.layerX
+      layerY = event.layerY
+    }
+
+    let percentCursorX = layerX / canvasWidth * 100
+    let percentCursorY = layerY / canvasHeight * 100
 
     this.x = canvas.width / 100 * percentCursorX
     this.y = canvas.height / 100 * percentCursorY
 
     // fallback function
     if (this.onmove) this.onmove()
-  })
+  }
+
+  canvas.addEventListener("mousemove", this.setPositions)
+  canvas.addEventListener("touchmove", this.setPositions)
 
   this.setStyles = styles => {
     this.styles.width = parseFloat(styles.width)
