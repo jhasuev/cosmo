eb67c852e172a2592e0180a6f396497ee76e17ee
Jamal Hasuev <metafra@mail.ru>
2021-01-17 22:03:41 +0300

    init

--- src/helpers/index.js
+++
@@ -1,4 +0,0 @@
-export function getRatioHeight(new_width, origin_width, origin_height) {
-    let width_percent = 100 / origin_width * new_width
-    return origin_height / 100 * width_percent
-}
--- src/index.js
+++ src/index.js
@@ -1,41 +1,9 @@
 import './scss/main.scss'
-import Game from './controller/Game.js'
 
-document.querySelectorAll("canvas").forEach(canvas => {
-  canvas.width = 720
-  canvas.height = 1280
-})
+let Game = {
+  init(){
+    console.log('init')
+  },
+}
 
-// все изображения, которые нужно загрузить
-document.querySelector('.js-game-images').innerHTML = `
-  <img src="assets/img/bg.jpg?v=${COSMO_VERSION}" id="bg-img">
-  <img src="assets/img/bullet2.png?v=${COSMO_VERSION}" id="bullet-img">
-  <img src="assets/img/enemy-starship-boss.png?v=${COSMO_VERSION}" id="enemy-boss-img">
-  
-  <img src="assets/img/enemy-starship-normal-die-sprite.png?v=${COSMO_VERSION}" id="enemy-normal-die-img">
-  <img src="assets/img/enemy-starship-stronger-die-sprite.png?v=${COSMO_VERSION}" id="enemy-stronger-die-img">
-  <img src="assets/img/enemy-starship-boss-die-sprite.png?v=${COSMO_VERSION}" id="enemy-boss-die-img">
-  
-  <img src="assets/img/enemy-starship-normal.png?v=${COSMO_VERSION}" id="enemy-normal-img">
-  <img src="assets/img/enemy-starship-stronger.png?v=${COSMO_VERSION}" id="enemy-stronger-img">
-  <img src="assets/img/user-starship.png?v=${COSMO_VERSION}" id="user-img">
-`
-
-let images = document.querySelectorAll(".js-game-images img")
-let must_load_images_num = images.length
-
-images.forEach(img => {
-  img.onload = () => {
-    must_load_images_num -= 1
-
-    // все картинки загружены (осталось 0 картинок для загрузки)
-    if (must_load_images_num === 0) {
-
-      // убираем прелоадер
-      document.getElementById("js-preloader").style.display = "none"
-
-      // запускаем игру
-      Game.init();
-    }
-  }
-})
+Game.init();
